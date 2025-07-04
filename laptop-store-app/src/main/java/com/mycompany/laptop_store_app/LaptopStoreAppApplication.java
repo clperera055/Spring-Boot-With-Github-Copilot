c3ace91c621b733ae5f072ac36b48e2ef6bd11e3
chathural1 <pereramlcl@gmail.com>
2025-07-04 17:59:28 +0530

    Initial Project Setup

---
+++ laptop-store-app/src/main/java/com/mycompany/laptop_store_app/LaptopStoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.laptop_store_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopStoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopStoreAppApplication.class, args);
+	}
+
+}
---
+++ laptop-store-app/src/test/java/com/mycompany/laptop_store_app/LaptopStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.laptop_store_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopStoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
